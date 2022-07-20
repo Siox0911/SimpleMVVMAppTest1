3c569d1ce9c518521fa116a1ab7356892a41cdd7
Siox0911 <Ronny.Witzgall@outlook.de>
2022-07-20 14:10:03 +0200

    Initial commit

---
+++ SimpleMVVMAppTest1/Model/SimplePerson.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace SimpleMVVMAppTest1.Model
+{
+    public class SimplePerson : ObservableObject
+    {
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { SetProperty(ref lastName, value); }
+        }
+
+        private string sureName;
+
+        public string SureName
+        {
+            get { return sureName; }
+            set { SetProperty(ref sureName, value); }
+        }
+
+        private ObservableCollection<string> childs;
+
+        public ObservableCollection<string> Childs
+        {
+            get { return childs; }
+            set { SetProperty(ref childs, value); }
+        }
+
+        public SimplePerson()
+        {
+            childs = new ObservableCollection<string>();
+            lastName = string.Empty;
+            sureName = string.Empty;
+        }
+    }
+}
---
+++ SimpleMVVMAppTest1/ViewModel/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Windows.Input;
+using System.Collections;
+
+namespace SimpleMVVMAppTest1.ViewModel
+{
+    public class MainViewModel : ObservableRecipient
+    {
+        private Model.SimplePerson person;
+
+        public Model.SimplePerson Person
+        {
+            get { return person; }
+            set { SetProperty(ref person, value); }
+        }
+
+        public ICommand ClearName { get; }
+        public ICommand DeleteChildName { get; }
+        public ICommand ResetData { get; }
+
+        public MainViewModel()
+        {
+            person = new Model.SimplePerson();
+            ClearName = new RelayCommand(ClearNameOfPerson);
+            DeleteChildName = new RelayCommand<IList>(DeleteNameOfChildFromList);
+            ResetData = new RelayCommand(GenerateSampleData);
+
+            GenerateSampleData();
+        }
+
+        private void GenerateSampleData()
+        {
+            Person.SureName = "Otto";
+            Person.LastName = "Bismark";
+            Person.Childs = new System.Collections.ObjectModel.ObservableCollection<string> { "Wilhelm", "Marie", "Herbert" };
+        }
+
+        private void DeleteNameOfChildFromList(IList? obj)
+        {
+            if (obj != null)
+            {
+                //ist etwas tricky
+                var copyOfSelectedItems = ((IList<object>)obj).ToList();
+
+                foreach (string item in copyOfSelectedItems)
+                {
+                    Person.Childs.Remove(item);
+                }
+            }
+        }
+
+        private void ClearNameOfPerson()
+        {
+            Person.LastName = String.Empty;
+            Person.SureName = String.Empty;
+        }
+    }
+}
